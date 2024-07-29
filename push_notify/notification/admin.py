16f4c57ca07460f40049f918d583e991042df196
Jeremiah-Katumo <jeremykatush@gmail.com>
2024-07-29 19:58:01 +0300

    static files

--- push_notify/notification/admin.py
+++ push_notify/notification/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from django import forms
+
+from . import models
 
 # Register your models here.
+class SendNotificationForm(forms.Form):
+    message = forms.CharField(label="Notification message", max_length=200)
+
+@admin.register(models.Notification)
+class NotificationAdmin(admin.ModelAdmin):
+    add_form_template = "admin/custom_add_form.html"
