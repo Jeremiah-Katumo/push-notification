f74f24d647f1b1dee539205eb080c3f2c03580b6
Jeremiah-Katumo <jeremykatush@gmail.com>
2024-07-30 13:03:50 +0300

    docker compose

--- push_notify/push_notify/settings.py
+++ push_notify/push_notify/settings.py
@@ -21,12 +21,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-r7p-bon(4kgw4ffh2wi9(&q4!=$#pxz%_ha7_6wqedgdpd#u)i'
+# SECRET_KEY = 'django-insecure-r7p-bon(4kgw4ffh2wi9(&q4!=$#pxz%_ha7_6wqedgdpd#u)i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
 
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = []
+
+SECRET_KEY = os.environ.get("SECRET_KEY")
+DEBUG = os.environ.get("DEBUG", default=False) == "True"
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(",")
 
 
 # Application definition
